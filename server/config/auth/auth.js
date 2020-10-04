de8f5bc4419d3035604592fa338a9f6832b78b48
Akshit Kakkar <Akshit88191@Mastek.Com>
2020-10-04 23:17:42 +0530

    updated gitignore to checkin server config

---
+++ server/config/auth/auth.js
@@ -0,0 +1,3 @@
+module.exports = {
+    API_AUTH_KEY: 'Basic a2Fra2FyLmFrc2hpdEB5YWhvby5jb206RkpvRE9yWXcyRUVSYXVCMElyblM1RUNF'
+}
---
+++ server/config/constants/urls.js
@@ -0,0 +1,6 @@
+module.exports = {
+    JIRA_DOMAIN: 'https://localhostapp.atlassian.net',
+    GET_JIRA_PROJECTS: '/rest/api/2/project',
+    SEARCH_JIRA_API: '/rest/api/2/search',
+    GET_TRANSITION: '/rest/api/2/issue/'
+}
